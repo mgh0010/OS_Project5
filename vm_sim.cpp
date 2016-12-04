60557a885f3f57fe104b2d647bef7738ccd17928
Michael <mgh0010@auburn.edu>
2016-12-03 19:00:54 -0600

    added more to vmsim

--- vm_sim.cpp
+++ vm_sim.cpp
@@ -2,11 +2,25 @@
 // Created by michael on 12/3/16.
 //
 
+#include <string>
+#include <fstream>
 #include "TLB.h"
 
+#define PHYSICAL_MEM_SIZE 256
+#define PAGE_TABLE_SIZE 256
+
 int main() {
-    // ;
     // Make TLB
     TLB * tlb = new TLB();
     setuptlb(tlb);
+    // Make page table
+    std::vector<page_t> page_table(PAGE_TABLE_SIZE);
+    // Make Physical Memory
+    std::vector<frame_t> physical_mem(PHYSICAL_MEM_SIZE);
+
+    std::string line;
+    //std::ifstream file ("./" + filename);
+    std::ifstream file ("/home/michael/ClionProjects/OS_Project5/testinput.txt");
+
+    if(file.is_open())
 }
